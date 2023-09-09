e8758a06aa32812caf72c4c380f2689a669c286c
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-09-09 16:01:38 +0530

    Item done

--- Back_End/src/lk/ijse/pos/servlet/util/ItemServlet.java
+++ Back_End/src/lk/ijse/pos/servlet/util/ItemServlet.java
@@ -1,8 +1,6 @@
 package lk.ijse.pos.servlet.util;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -44,8 +42,142 @@ break;
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+        String description = req.getParameter("description");
+        String unitPrice = req.getParameter("unitPrice");
+        String qtyOnHand = req.getParameter("qtyOnHand");
+        resp.addHeader("Access-Control-Allow-Origin","*");
 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posapi", "root", "1234");
 
+            PreparedStatement pstm = connection.prepareStatement("insert into Item values(?,?,?,?)");
+            pstm.setObject(1,code);
+            pstm.setObject(2,description);
+            pstm.setObject(3,unitPrice);
+            pstm.setObject(4,qtyOnHand);
+            boolean b = pstm.executeUpdate() > 0;
+            if (b){
+                JsonObjectBuilder responseObject = Json.createObjectBuilder();
+                responseObject.add("state","Ok");
+                responseObject.add("message","Successfully added..!");
+                responseObject.add("data","");
+                resp.getWriter().print(responseObject.build());
+            }
+        } catch (ClassNotFoundException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Ok");
+            error.add("message",e.getLocalizedMessage());
+            error.add("data","");
+//            resp.setStatus(500);
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(error.build());
+        }catch (SQLException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("data","");
+//            resp.setStatus(400);
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(error.build());
+        }
+    }
+
+    //    query string
+//    JSON
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+        resp.setContentType("application/json");
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posapi", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("delete from Item where ItemCode=?");
+            pstm.setObject(1,code);
+            boolean b = pstm.executeUpdate() > 0;
+            if (b) {
+                JsonObjectBuilder rjo = Json.createObjectBuilder();
+                rjo.add("state","Ok");
+                rjo.add("message","Successfully Deleted..!");
+                rjo.add("data","");
+                resp.getWriter().print(rjo.build());
+            }else {
+                throw new RuntimeException("There is no Item for that ID..!");
+            }
+        } catch (RuntimeException e) {
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(rjo.build());
+        }catch (ClassNotFoundException | SQLException e){
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(rjo.build());
+        }
+    }
+
+    //    query string
+//    JSON
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject item = reader.readObject();
+        String code = item.getString("code");
+        String description = item.getString("description");
+        String unitPrice = item.getString("unitPrice");
+        String qtyOnHand = item.getString("qtyOnHand");
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/posapi", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("update Item set ItemName=?,UnitPrice=?,ItemQty=? where ItemCode=?");
+            pstm.setObject(4,code);
+            pstm.setObject(1,description);
+            pstm.setObject(2,unitPrice);
+            pstm.setObject(3,qtyOnHand);
+            boolean b = pstm.executeUpdate() > 0;
+            if (b){
+                JsonObjectBuilder responseObject = Json.createObjectBuilder();
+                responseObject.add("state","Ok");
+                responseObject.add("message","Successfully Updated..!");
+                responseObject.add("data","");
+                resp.getWriter().print(responseObject.build());
+            }else{
+                throw new RuntimeException("Wrong ID, Please check the ID..!");
+            }
+
+        } catch (RuntimeException e) {
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(rjo.build());
+        }catch (ClassNotFoundException | SQLException e){
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(rjo.build());
+        }
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        resp.addHeader("Access-Control-Allow-Methods","DELETE,PUT");
+        resp.addHeader("Access-Control-Allow-Headers","content-type");
     }
 }
--- Front_End/controllers/Item.js
+++ Front_End/controllers/Item.js
@@ -24,3 +24,70 @@ function getAllItems(){
     },
     });
 }
+// add Item
+$("#itemSave-btn").click(function () {
+    let formData = $("#item-form px-2").serialize();
+    $.ajax({
+        url: baseUrl+"item",
+        method: "post",
+        data: formData,
+        dataType: "json",
+        success: function (res) {
+            console.log("success Method Invoked");
+            console.log(res);
+            alert(res.message);
+            getAllItems();
+        },
+        error: function (error) {
+            console.log("Error Method Invoked");
+            console.log(JSON.parse(error.responseText));
+            alert(JSON.parse(error.responseText).message);
+        }
+    });
+});
+
+//delete Item
+$("#itemDelete-btn").click(function () {
+    let code = $("#txtItemCode").val();
+    $.ajax({
+        url: baseUrl+"item?code=" + code,
+        method: "delete",
+        success: function (resp) {
+            getAllItems();
+            alert(resp.message);
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+});
+
+//update Item
+$("#itemUpdated-btn").click(function () {
+    let code = $('#txtItemCode').val();
+    let description = $('#txtItemName').val();
+    let qtyOnHand = $('#txtItemQTY').val();
+    let unitPrice = $('#txtItemPrice').val();
+    var ItemOb = {
+        code: code,
+        description: description,
+        qtyOnHand: qtyOnHand,
+        unitPrice: unitPrice
+    }
+    $.ajax({
+        url: baseUrl+"item",
+        method: "put",
+        contentType: "application/json",
+        data: JSON.stringify(ItemOb),
+        dataType: "json",
+        success: function (resp) {
+            getAllItems();
+            alert(resp.message);
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+});
