1558f563a67fb15b8f73b349823f24bf83872589
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-09-10 19:38:33 +0530

    Item DAO

--- Back_End/src/lk/ijse/pos/servlet/bo/BOFactory.java
+++ Back_End/src/lk/ijse/pos/servlet/bo/BOFactory.java
@@ -1,6 +1,8 @@
 package lk.ijse.pos.servlet.bo;
 
 import lk.ijse.pos.servlet.bo.custom.impl.CustomerBOImpl;
+import lk.ijse.pos.servlet.bo.custom.impl.ItemBOImpl;
+
 public class BOFactory {
     private static BOFactory boFactory;
 
@@ -21,6 +23,8 @@ public class BOFactory {
         switch (types) {
             case CUSTOMER:
                 return new CustomerBOImpl();
+            case ITEM:
+                return new ItemBOImpl();
             default:
                 return null;
         }
--- Back_End/src/lk/ijse/pos/servlet/bo/custom/impl/ItemBOImpl.java
+++ Back_End/src/lk/ijse/pos/servlet/bo/custom/impl/ItemBOImpl.java
@@ -1,4 +1,47 @@
 package lk.ijse.pos.servlet.bo.custom.impl;
 
-public class ItemBOImpl {
+import lk.ijse.pos.servlet.bo.SuperBO;
+import lk.ijse.pos.servlet.bo.custom.ItemBO;
+import lk.ijse.pos.servlet.dao.DAOFactory;
+import lk.ijse.pos.servlet.dao.custom.ItemDAO;
+import lk.ijse.pos.servlet.dto.ItemDTO;
+import lk.ijse.pos.servlet.entity.Item;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemBOImpl implements ItemBO, SuperBO {
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ITEM);
+
+    @Override
+    public ArrayList<ItemDTO> getAllItems(Connection connection) throws SQLException, ClassNotFoundException {
+        ArrayList<Item> all = itemDAO.getAll(connection);
+        ArrayList<ItemDTO> allItems = new ArrayList<>();
+        for (Item item :
+                all) {
+            allItems.add(new ItemDTO(item.getCode(), item.getName(), item.getQty(), item.getPrice()));
+        }
+        return allItems;
+    }
+
+    @Override
+    public boolean deleteItems(Connection connection, String id) throws SQLException, ClassNotFoundException {
+        return itemDAO.delete(connection, id);
+    }
+
+    @Override
+    public boolean saveItem(Connection connection, ItemDTO item) throws SQLException, ClassNotFoundException {
+        return itemDAO.save(connection, new Item(item.getCode(), item.getName(), item.getQty(), item.getPrice()));
+    }
+
+    @Override
+    public boolean updateItem(Connection connection, ItemDTO item) throws SQLException, ClassNotFoundException {
+        return itemDAO.update(connection, new Item(item.getCode(), item.getName(), item.getQty(), item.getPrice()));
+    }
+
+    @Override
+    public boolean exitsItem(Connection connection, String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
 }
